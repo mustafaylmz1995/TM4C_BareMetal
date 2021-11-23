de454786be8e14d2141ca37900361399edd602ee
mustafaylmz1995 <mustafaylmz1995@gmail.com>
2021-11-23 16:45:42 +0300

    SPI eklendi denenmeli

--- 29_SPI_basic/main.c
+++ 29_SPI_basic/main.c
@@ -1,18 +1,48 @@
 #include "TM4C129.h"                    // Device header
 
+void delay(unsigned int time);
+void SSI_init(void);
+void SSI2Write(unsigned char data);
 
 int main(){
 	
 	
+	SSI_init();
+	unsigned char i;
 	while(1){
 		
-		
-		
+		for(i = 'A'; i<='z'; i++){
+			SSI2Write(i);
+			
+		}
+	
 	}
 	
 	
 }
 
+void delay(unsigned int time){
+	
+	unsigned int j=0;
+	SYSCTL->RCGCTIMER |= 0x04; //TIMER2A
+	while( (SYSCTL->PRTIMER &(1U<<2)) != (1U<<2)) {}; //Allow time to finish activating 
+	
+	TIMER2->CTL &= ~(1U<<0);
+	TIMER2->CFG = 0x04;
+
+	TIMER2->TAMR = 0x02;
+	TIMER2->TAILR = 16-1;
+
+	TIMER2->ICR =0x01;
+	TIMER2->CTL |= (1U<<0);
+		
+	for (j=0; j<time; j++){
+		while((TIMER2->RIS &0x1) == 0x00){	};
+		TIMER2->ICR = 0x01;
+	}
+	
+
+}
 
 void SSI_init(void){
 	
@@ -37,10 +67,127 @@ void SSI_init(void){
 	/*
 	*		SPI Master, CPOL = 0, CPHA = 0, Clk = 4 MHz, 16 bit data
 	*/
+	//-----------------------------------------------------------	
+	/*ensure that the SSE bit in the SSICR1 register is clear before making any configuration changes*/
+	/*
+		Bit		Description
+		11		Stop Frame : 0 No change is transmission status.
+					This bit is applicable when MODE is set to Advanced, Bi- or Quad- SSI.
+					This bit is inserted into bit 12 of the TXFIFO data entry by the QSSI
+					module.
+		
+		10  	FSS Hold Frame: 0
+					Pulse SSInFss at every byte (the DSS bit in the SSICR0 register
+					must be set to 0x7 (data size 8 bits) in this configuration)
+		
+		9			High Speed Clock Enable: 0 Use Input Clock 
+					High speed clock enable is available only when operating as a master.
+		
+		8			SSI Diretion of Operation : 0 TX (Transmit Mode) write direction
+		
+		7:6		SSI Mode : 0x0 Legacy SSI mode
+		
+		2			SSI Master/Slave Select :  The QSSI is configured as a master.
+					This bit selects Master or Slave mode and can be modified only when
+					the QSSI is disabled (SSE=0).
+	
+		1			SSI Synchronous Serial Port Enable : 0 QSSI operation is disabled.
+		
+		0			SSI Loopback Mode : 0 Normal serial port operation enabled.
+		
+		
+		*/		
+	SSI2->CR1 = 0x00; // disable SS2
+	
+	
+	/* clock source */
+	/*
+		Bit		Description
+		3:0		SSI Baud Clock Source: 0x0 System clock (based on clock source and divisor factor
+					programmed in RSCLKCFG register in the System Control Module)
+					Specifies the source that generates for the QSSI baud clock
+	*/
+	SSI2->CC =0x00;
+	
+	
+	/*Configure the clock prescale divisor*/
+	/*
+		Bit   Description
+		7:0		CPSDVSR: SSI Clk Prescale Divisor
+					This value must be an even number from 2 to 254, depending on the
+					frequency of SSInClk. The LSB always returns 0 on reads	
+	*/
+	/* SSInClk = SysClk / (CPSDVSR * (1 + SCR)) */
+	SSI2->CPSR = 0x02; //prescaler divided by 2 8MHz
+	
+	
+	/* Configure serial Clk Rate, Clk Phase and Polarity, Protocol Mode and Data Size */
+	/*
+		Bit   Description
+		15:8	Serial Clk Rate : 
+					This bit field is used to generate the transmit and receive bit rate of the
+					SSI. The bit rate is:	BR=SysClk/(CPSDVSR * (1 + SCR)) where CPSDVSR is an 
+					even value from 2-254 programmed in the SSICPSR register, and SCR is a value from 0-255.
+		7     Serial Clk Phase : 0 Data is captured on the first clock edge transition.
+		6			Serial Clk Polarity :  A steady state Low value is placed on the SSInClk pin.
+		5:4		SSI Frame Format Select: 0x0 Freescale SPI Frame Format
+		3:0   SSI Data Size : 0x7 8-bit data
+
+	*/
+	SSI2->CR0 = 0x7; //8 MHz SSI clk, SPImode, 8 bit data
+	
+	SSI2->CR1 |= (1U<<0); //Enable SSI2
 		
 		
 }
 
+
+void SSI2Write(unsigned char data){
+	GPIOF_AHB->DATA &= ~(1U<<0);
+	/*Status Register*/
+	/*
+		Bit   Description
+		4			SSI Busy Bit
+					0 The QSSI is idle.
+					1 The QSSI is currently transmitting and/or receiving a frame, or the transmit FIFO is not empty.
+		
+		3			SSI Receive FIFO Full
+					0 The receive FIFO is not full.
+					1 The receive FIFO is full.
+	
+		2			SSI Receive FIFO Not Empty
+					0 The receive FIFO is empty.
+					1 The receive FIFO is not empty
+	
+		1			SSI Transmit FIFO Not Full
+					0 The transmit FIFO is full.
+					1 The transmit FIFO is not full
+	
+		0			SSI Transmit FIFO Empty
+					0 The transmit FIFO is not empty.
+					1 The transmit FIFO is empty.
+	
+	*/
+	while( (SSI2->SR & (1U<<1)) == 0) {}; //wait for not full
+		
+	/*
+		SSI Receive/Transmit Data
+		A read operation reads the receive FIFO. A write operation writes the transmit FIFO.
+		Software must right-justify data when the QSSI is programmed for a data size that is 
+		less than 16 bits. Unused bits at the top are ignored by the transmit logic. 
+		The receive logic automatically right-justifies the data.
+				
+		*/
+		
+	SSI2->DR = data; //16 bits wide
+	
+	while( (SSI2->SR & (1U<<4)) == 0) {};//wait for transmission completed
+	
+	GPIOF_AHB->DATA |= (1U<<0);
+	
+	delay(1e6);
+		
+}
 
 
 
